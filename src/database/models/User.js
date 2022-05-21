8f9c8ebad23aeb4fff86b9fc4eb56061e7f2f52f
Elías Heresi <elias.heresi@gmail.com>
2022-05-21 18:26:00 +0200

    Corrected eslint in user.js

--- src/database/models/User.js
+++ src/database/models/User.js
@@ -31,7 +31,7 @@ const UserSchema = new Schema(
   },
   {
     toObject: {
-      transform: (null, returnObject) => {
+      transform: (document, returnObject) => {
         returnObject.id = returnObject._id;
         delete returnObject.__v;
         delete returnObject._id;
